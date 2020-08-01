0b2dea90793190ddada41cacc2be58fb2d84a447
aiji42 <aiji42@users.noreply.github.com>
2020-08-01 02:22:59 +0000

    deploy: 2c72be8847a0dd6ce1d511f6d755b7c90feb8a72

---
+++ precache-manifest.9913356c55406ff89f4b95045599666d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d658f9fd9debfc4923a0d1e26e4a9b3",
+    "url": "/use-cors-state/index.html"
+  },
+  {
+    "revision": "289b07e04a3881dd0a55",
+    "url": "/use-cors-state/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "83b7664e6e0c062bbea4",
+    "url": "/use-cors-state/static/js/2.04a1fff9.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/use-cors-state/static/js/2.04a1fff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "289b07e04a3881dd0a55",
+    "url": "/use-cors-state/static/js/main.3d86cc24.chunk.js"
+  },
+  {
+    "revision": "cdd803ee18070fb0543c",
+    "url": "/use-cors-state/static/js/runtime-main.f372a22f.js"
+  }
+]);
